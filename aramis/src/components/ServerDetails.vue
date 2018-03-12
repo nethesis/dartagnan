8de519dd58182b22e1d3e1a7929f4635138c1a4d
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2018-03-12 12:56:43 +0100

    aramis. added resources details in server

--- aramis/src/components/ServerDetails.vue
+++ aramis/src/components/ServerDetails.vue
@@ -163,42 +163,6 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
           <div class="card-pf card-pf-accented">
             <h2 class="card-pf-title">
-              {{$t('servers.resources')}}
-            </h2>
-            <div class="card-pf-body">
-              <p v-if="!isLoadingInventory">[card contents]</p>
-              <div v-if="isLoadingInventory" class="spinner spinner-sm"></div>
-            </div>
-            <div class="card-pf-footer">
-              <div class="dropdown card-pf-time-frame-filter">
-                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                  Last 30 Days
-                  <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-right" role="menu">
-                  <li class="selected">
-                    <a href="#">Last 30 Days</a>
-                  </li>
-                  <li>
-                    <a href="#">Last 60 Days</a>
-                  </li>
-                  <li>
-                    <a href="#">Last 90 Days</a>
-                  </li>
-                </ul>
-              </div>
-              <p>
-                <a href="#" class="card-pf-link-with-icon">
-                  <span class="pficon pficon-add-circle-o"></span>Add New Cluster
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-          <div class="card-pf card-pf-accented">
-            <h2 class="card-pf-title">
               {{$t('servers.alerts_details')}}
             </h2>
             <div class="card-pf-body">
@@ -227,6 +191,244 @@
             </div>
           </div>
         </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+          <div class="card-pf card-pf-accented">
+            <h2 class="card-pf-title">
+              {{$t('servers.resources')}}
+            </h2>
+            <div class="card-pf-body resources-container">
+              <div v-if="!isLoadingInventory && !server.inventory" class="alert alert-warning alert-dismissable">
+                <span class="pficon pficon-warning-triangle-o"></span>
+                <strong>{{$t('servers.inventory_not_available')}}</strong>. {{$t('servers.inventory_not_available_desc')}}.
+              </div>
+              <ul v-if="!isLoadingInventory && server.inventory" class="nav nav-tabs nav-tabs-pf" id="myTab" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" id="system-tab-parent" data-toggle="tab" href="#system-tab" role="tab" aria-controls="system"
+                    aria-selected="true">{{$t('servers.system')}}</a>
+                </li>
+                <li @click="initMemoryCharts()" class="nav-item">
+                  <a class="nav-link" id="memory-tab-parent" data-toggle="tab" href="#memory-tab" role="tab" aria-controls="memory" aria-selected="false">{{$t('servers.memory')}}</a>
+                </li>
+                <li @click="initStorageCharts()" class="nav-item">
+                  <a class="nav-link" id="storage-tab-parent" data-toggle="tab" href="#storage-tab" role="tab" aria-controls="storage" aria-selected="false">{{$t('servers.storage')}}</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" id="network-tab-parent" data-toggle="tab" href="#network-tab" role="tab" aria-controls="network" aria-selected="false">{{$t('servers.network')}}</a>
+                </li>
+              </ul>
+              <div v-if="!isLoadingInventory && server.inventory" class="tab-content" id="myServerResourceContent">
+                <div class="tab-pane fade active" id="system-tab" role="tabpanel" aria-labelledby="system-tab">
+                  <div class="container-fluid container-cards-pf">
+                    <div class="row row-cards-pf">
+                      <!-- OS -->
+                      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-6 resources-panel">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="icon-header-panel">
+                                <span class="pficon pficon-running"></span>
+                              </a>{{$t('servers.os')}}</h3>
+                          </div>
+                          <div class="panel-body">
+                            <span class="details-info " ng-class="">{{server.inventory.os.name}}</span>
+                            <div class="text-right">{{$t('servers.vendor')}}:
+                              <b>
+                                <span class="">{{server.inventory.os.family}}</span>
+                              </b>
+                            </div>
+                            <div class="text-right">{{$t('servers.release')}}:
+                              <b class="">{{server.inventory.os.release.full}}</b>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- Kernel -->
+                      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-6 resources-panel">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="icon-header-panel">
+                                <span class="fa fa-cogs"></span>
+                              </a>{{$t('servers.kernel')}}</h3>
+                          </div>
+                          <div class="panel-body">
+                            <span class="details-info " ng-class="">{{server.inventory.kernel}}</span>
+                            <div class="text-right">{{$t('servers.release')}}:
+                              <b>
+                                <span class="">{{server.inventory.kernelrelease}}</span>
+                              </b>
+                            </div>
+                            <div class="text-right">{{$t('servers.architecture')}}:
+                              <b class="">{{server.inventory.os.architecture}}</b>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- Machine -->
+                      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-6 resources-panel">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="icon-header-panel">
+                                <span class="pficon pficon-screen"></span>
+                              </a>{{$t('servers.machine')}}</h3>
+                          </div>
+                          <div class="panel-body">
+                            <span class="details-info " ng-class="">{{server.inventory.virtual}}</span>
+                            <div class="text-right ">{{$t('servers.uuid')}}:
+                              <b>
+                                <span class="">{{server.inventory.dmi.product.uuid}}</span>
+                              </b>
+                            </div>
+                            <div class="text-right resource-info-details"></div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- CPU -->
+                      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-6 resources-panel">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="icon-header-panel">
+                                <span class="pficon pficon-cpu"></span>
+                              </a>{{$t('servers.cpu')}}</h3>
+                          </div>
+                          <div class="panel-body">
+                            <span class="details-info" ng-class="">{{server.inventory.processors.count}} Core</span>
+                            <div class="text-right">{{$t('servers.model')}}:
+                              <b>
+                                <span class="">{{server.inventory.processors.models[0]}}</span>
+                              </b>
+                            </div>
+                            <div class="text-right resource-info-details"></div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- BIOS -->
+                      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-6 resources-panel">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h3 class="panel-title">
+                              <a class="icon-header-panel">
+                                <span class="pficon pficon-enterprise"></span>
+                              </a>{{$t('servers.bios')}}</h3>
+                          </div>
+                          <div class="panel-body">
+                            <span class="details-info " ng-class="">{{server.inventory.dmi.bios.version}}</span>
+                            <div class="text-right">{{$t('servers.vendor')}}:
+                              <b>
+                                <span class="">{{server.inventory.dmi.bios.vendor}}</span>
+                              </b>
+                            </div>
+                            <div class="text-right resource-info-details"></div>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane fade" id="memory-tab" role="tabpanel" aria-labelledby="memory-tab">
+                  <div class="row row-cards-pf">
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 resources-panel">
+                      <div class="panel panel-default">
+                        <div class="panel-heading">
+                          <h3 class="panel-title">
+                            <a class="icon-header-panel">
+                              <span class="pficon pficon-memory"></span>
+                            </a>{{$t('servers.ram')}}</h3>
+                        </div>
+                        <div class="panel-body">
+                          <div id="ram-chart" class="text-center"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 resources-panel">
+                      <div class="panel panel-default">
+                        <div class="panel-heading">
+                          <h3 class="panel-title">
+                            <a class="icon-header-panel">
+                              <span class="fa fa-exchange"></span>
+                            </a>{{$t('servers.swap')}}</h3>
+                        </div>
+                        <div class="panel-body">
+                          <div id="swap-chart" class="text-center"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane fade" id="storage-tab" role="tabpanel" aria-labelledby="storage-tab">
+                  <div class="row row-cards-pf">
+                    <div v-for="(k, v) in server.inventory.mountpoints" v-bind:key="v" class="col-xs-12 col-sm-6 col-md-6 col-lg-6 resources-panel">
+                      <div class="panel panel-default">
+                        <div class="panel-heading">
+                          <h3 class="panel-title">
+                            <a class="icon-header-panel">
+                              <span class="fa fa-hdd-o"></span>
+                            </a>{{v}}</h3>
+                        </div>
+                        <div class="panel-body">
+                          <div :id="'mount-chart-'+parseMount(v)" class="text-center"></div>
+                          <div class="text-right ">{{$t('servers.total')}}:
+                            <b>
+                              <span class="">{{server.inventory.mountpoints[v].size_bytes | byteFormat}}</span>
+                            </b>
+                          </div>
+                          <div class="text-right ">{{$t('servers.device')}}:
+                            <b>
+                              <span class="">{{server.inventory.mountpoints[v].device}}</span>
+                            </b>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane fade" id="network-tab" role="tabpanel" aria-labelledby="network-tab">
+                  <div class="row row-cards-pf">
+                    <div v-if="e.type != 'xdsl-disabled'" v-for="(e, k) in server.inventory.esmithdb.networks" v-bind:key="k" class="col-xs-12 col-sm-6 col-md-4 col-lg-6 resources-panel">
+                      <div class="panel panel-default">
+                        <div class="panel-heading">
+                          <h3 class="panel-title">
+                            <a class="icon-header-panel">
+                              <span class="pficon pficon-network"></span>
+                            </a>{{$t('servers.interface')}} {{e.name}}</h3>
+                        </div>
+                        <div class="panel-body">
+                          <div>
+                            <span>{{$t('servers.ip')}}</span>
+                            <span class="details-info" ng-class="">{{e.props.ipaddr || '-'}}</span>
+                          </div>
+                          <div>
+                            <span>{{$t('servers.netmask')}}</span>
+                            <span class="details-info" ng-class="">{{e.props.netmask || '-'}}</span>
+                          </div>
+                          <div>
+                            <span>{{$t('servers.gateway')}}</span>
+                            <span class="details-info" ng-class="">{{e.props.gateway || '-'}}</span>
+                          </div>
+                          <div class="text-right">{{$t('servers.type')}}:
+                            <b>
+                              <span class="">{{e.type}}</span>
+                            </b>
+                          </div>
+                          <div class="text-right">{{$t('servers.role')}}:
+                            <b>
+                              <span :class="e.props.role">{{e.props.role}}</span>
+                            </b>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </div>
@@ -238,6 +440,7 @@
   import StorageService from './../services/storage';
   import UtilService from './../services/util';
   import _ from 'lodash'
+  import c3 from 'patternfly/node_modules/c3'
 
   import RenewButton from './directives/RenewButton.vue';
   import DeleteServer from './directives/DeleteServer.vue';
@@ -337,6 +540,10 @@
           this.server.inventory = success.body.data
           this.server.inventory.timestamp = success.body.timestamp
           this.isLoadingInventory = false
+          // init tab
+          setTimeout(function () {
+            $('#system-tab-parent').click()
+          }, 500)
         }, function (error) {
           console.error(error)
           this.isLoadingInventory = false
@@ -369,6 +576,97 @@
           console.error(error)
           this.isLoadingHeartbeat = false
         });
+      },
+      initMemoryCharts() {
+        var c3ChartDefaults = $().c3ChartDefaults();
+        var ramConfig = c3ChartDefaults.getDefaultDonutConfig('A');
+        var swapConfig = c3ChartDefaults.getDefaultDonutConfig('A');
+        ramConfig.bindto = '#ram-chart';
+        swapConfig.bindto = '#swap-chart';
+        ramConfig.data = {
+          type: "donut",
+          columns: [
+            ["Used", this.server.inventory.memory.system.used_bytes],
+            ["Available", this.server.inventory.memory.system.available_bytes],
+          ],
+          groups: [
+            ["used", "available"]
+          ],
+          order: null
+        };
+        swapConfig.data = {
+          type: "donut",
+          columns: [
+            ["Used", this.server.inventory.memory.swap.used_bytes],
+            ["Available", this.server.inventory.memory.swap.available_bytes],
+          ],
+          groups: [
+            ["used", "available"]
+          ],
+          order: null
+        };
+        ramConfig.size = {
+          width: 180,
+          height: 180
+        };
+        swapConfig.size = {
+          width: 180,
+          height: 180
+        };
+
+        ramConfig.tooltip = {
+          contents: $().pfGetUtilizationDonutTooltipContentsFn('GB')
+        };
+        swapConfig.tooltip = {
+          contents: $().pfGetUtilizationDonutTooltipContentsFn('GB')
+        };
+
+        c3.generate(ramConfig);
+        c3.generate(swapConfig);
+        $().pfSetDonutChartTitle("#ram-chart", this.$options.filters.byteFormat(this.server.inventory.memory.system.used_bytes),
+          " Used");
+        $().pfSetDonutChartTitle("#swap-chart", this.$options.filters.byteFormat(this.server.inventory.memory.swap.used_bytes),
+          " Used");
+      },
+      parseMount(value) {
+        return 'm' + value.substring(1);
+      },
+      initStorageCharts() {
+        var c3ChartDefaults = $().c3ChartDefaults();
+        var mountConfig = c3ChartDefaults.getDefaultDonutConfig('A');
+
+        for (var m in this.server.inventory.mountpoints) {
+          var mount = this.server.inventory.mountpoints[m]
+
+          mountConfig.bindto = '#mount-chart-' + this.parseMount(m);
+          mountConfig.data = {
+            type: "donut",
+            columns: [
+              ["Used", mount.used_bytes],
+              ["Available", mount.available_bytes],
+            ],
+            groups: [
+              ["used", "available"]
+            ],
+            order: null
+          };
+
+          mountConfig.size = {
+            width: 180,
+            height: 180
+          };
+
+
+          mountConfig.tooltip = {
+            contents: $().pfGetUtilizationDonutTooltipContentsFn('GB')
+          };
+
+          c3.generate(mountConfig);
+          $().pfSetDonutChartTitle("#mount-chart-" + this.parseMount(m), this.$options.filters.byteFormat(mount
+              .used_bytes),
+            " Used");
+        }
+
       }
     }
   }
