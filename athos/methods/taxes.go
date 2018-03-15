9296fa7e9d5a7e4dff62522ea9b88152b4a3457d
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2018-03-15 10:41:16 +0100

    athos. add missing taxes file

---
+++ athos/methods/taxes.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2018 Nethesis S.r.l.
+ * http://www.nethesis.it - info@nethesis.it
+ *
+ * This file is part of Dartagnan project.
+ *
+ * Dartagnan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * Dartagnan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dartagnan.  If not, see COPYING.
+ *
+ */
+
+package methods
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/nethesis/dartagnan/athos/database"
+	"github.com/nethesis/dartagnan/athos/models"
+)
+
+func GetTaxes(c *gin.Context) {
+	var taxes []models.Tax
+
+	db := database.Database()
+	db.Find(&taxes)
+	defer db.Close()
+
+	if len(taxes) == 0 {
+		db.Close()
+		c.JSON(http.StatusNotFound, gin.H{"message": "no taxes found!"})
+		return
+	}
+
+
+	c.JSON(http.StatusOK, taxes)
+}
+
